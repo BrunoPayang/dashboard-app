e8827241f0563c7ba153cd6760d349aba4a99f7d
Ganontha <bganontha@gmail.com>
2025-09-18 21:58:47 +0200

    fized issue with refreshing error leading not not found

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as BrowserRouter } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
 import { store } from './features/store';
